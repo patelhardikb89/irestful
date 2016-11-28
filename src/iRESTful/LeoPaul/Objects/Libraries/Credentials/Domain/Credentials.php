8b08894d51d44f1576fac9ecff4f659d58981bc3
Steve Rodrigue <steve@irestful.com>
2016-11-28 05:12:27 -0500

    Removed unnecessary code.

--- src/iRESTful/LeoPaul/Objects/Libraries/Credentials/Domain/Credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace iRESTful\LeoPaul\Objects\Libraries\Credentials\Domain;
-
-interface Credentials {
-    public function getUsername();
-    public function getPassword();
-}
--- src/iRESTful/LeoPaul/Objects/Libraries/Credentials/Domain/Exceptions/CredentialsException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace iRESTful\LeoPaul\Objects\Libraries\Credentials\Domain\Exceptions;
-
-final class CredentialsException extends \Exception {
-    const CODE = 1;
-    public function __construct($message, \Exception $parentException = null) {
-        parent::__construct($message, self::CODE, $parentException);
-    }
-}
